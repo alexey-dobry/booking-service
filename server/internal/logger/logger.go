ac70cb0aa6d64c951563944bb97190e0b12ca3f3
Alex <Alex@example.com>
2025-01-07 11:32:19 +0300

    feat: added logger

--- server/internal/logger/logger.go
+++ server/internal/logger/logger.go
@@ -1,1 +1,47 @@
 package logger
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	oz "github.com/go-ozzo/ozzo-log"
+)
+
+type Logger struct {
+	ozzo       *oz.Logger
+	logDirPath string
+}
+
+func newLogger() *Logger {
+	var logger Logger
+	logger.ozzo = oz.NewLogger()
+	logger.logDirPath = "../../logs"
+
+	err := os.Mkdir("../../logs", os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		log.Fatalf("cannot create logger path: %s", err)
+	}
+
+	logFile, err := os.OpenFile(filepath.Join(logger.logDirPath, "server.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("cannot open logger file: %s", err)
+	}
+
+	targetFile := oz.NewFileTarget()
+	targetFile.FileName = logFile.Name()
+
+	logger.ozzo.Targets = append(logger.ozzo.Targets, targetFile)
+
+	logger.ozzo.Open()
+
+	return &logger
+}
+
+func (l *Logger) Error(errorMsg string) {
+	l.ozzo.Error(errorMsg)
+}
+
+func (l *Logger) Debug(debugMsg string) {
+	l.ozzo.Debug(debugMsg)
+}
