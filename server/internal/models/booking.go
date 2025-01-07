e956dad06c711b604b747916b3ace2ee35adfe3b
Alex <Alex@example.com>
2025-01-07 12:22:56 +0300

    fix: validation

--- server/internal/models/booking.go
+++ server/internal/models/booking.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Booking struct {
-	Id        int       `json:"id" validate:"required"`
-	UserId    int       `json:"user_id" validate:"required"`
+	Id        int       `json:"id" validate:"required,min=1,max=6"`
+	UserId    int       `json:"user_id" validate:"required,min=1,max=6"`
 	StartTime time.Time `json:"start_time" validate:"required"`
 	EndTime   time.Time `json:"end_time" validate:"required"`
 }
--- server/internal/models/user.go
+++ server/internal/models/user.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	Id        int       `json:"id" validate:"required"`
-	Username  string    `json:"username" validate:"required"`
-	Password  string    `json:"password" validate:"required"`
+	Id        int       `json:"id" validate:"required,min=1,max=6"`
+	Username  string    `json:"username" validate:"required,ascii,min=10,max64"`
+	Password  string    `json:"password" validate:"required,ascii,min=10,max64"`
 	CreatedAt time.Time `json:"created_at" validate:"required"`
 	UpdatedAt time.Time `json:"updated_at" validate:"required"`
 }
